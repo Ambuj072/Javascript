eda5690b7adbdacd5cf43c336f5edadfe3f3afc4
Ambuj Dubey <ambujdubey072@gmail.com>
2024-10-23 23:54:27 +0530

    var let const

---
+++ start/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const name="Ambuj dubey"
+var email="ambujdubey072@gmail.com"
+let pNumber=1234567899;
+city="prayagraj"
+let state;
+
+//name="anuj" //  we can not assign to const variable
+
+email="ambujdubey071@gamil.com"
+pNumber=9987654321
+city="Allahabad"
+
+console.log(email);
+console.table([email,pNumber,city,state])
+
+/*
+Do not prefer var because of issue block scope and function scope
+*/
