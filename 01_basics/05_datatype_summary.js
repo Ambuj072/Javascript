e83f64d17b4b082dcf098303662d9bda9d9fe1c8
Ambuj Dubey <ambujdubey072@gmail.com>
2024-10-28 18:15:19 +0530

    heap and stack

--- 01_basics/05_datatype_summary.js
+++ 01_basics/05_datatype_summary.js
@@ -24,7 +24,7 @@ let id=Symbol("abc")
 let anotherId=Symbol("def")
 console.log(id==anotherId)//false because unique
 
-//2. Non primitive
+//2. Non primitive(Reference )
 // object,function,Array
 
 let myObject={
@@ -48,5 +48,32 @@ console.log(typeof myFuntion)
  Null=> it's type is object
  undefined=> it's type is undefined
  Arrays=> it's type is object
- function=>it will return function but it is also object
+ function=>it will return function but it is also object type
 */
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//primitive(stack)  Non-Primitive(heap)
+
+let myName="Ambuj"
+let anotherName=myName
+anotherName="Ambuj dubey"
+console.log(myName);//Ambuj
+console.log(anotherName);//Ambuj dubey
+//in this above programe it is copy the value then in the copy we are modifing the value
+
+
+let intialDetails={
+    name:"Ambuj dubey",
+    course:"B-tech"
+}
+let finalDetails=intialDetails
+finalDetails.course="B-tech(cse)"
+console.log(intialDetails.course);//B-tech(cse)
+console.log(finalDetails.course);//B-tech(cse)
+// in this above details we are not coping the value . we are passing the reference then if we are modifing the new value then old value will also change 
+
+
+
+
